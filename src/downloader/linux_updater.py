b20a1e5897923a93078d288cc01fe587f24d8cdc
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-11-16 12:48:27 +0100

    Correct hash for 7z

--- src/downloader/linux_updater.py
+++ src/downloader/linux_updater.py
@@ -78,7 +78,7 @@ class LinuxUpdater:
             file_downloader.queue_file({
                 'delete': [],
                 'url': 'https://github.com/MiSTer-devel/SD-Installer-Win64_MiSTer/raw/master/7za.gz',
-                'hash': 'ed1ad5185fb1de55cd7fd506b3c6c699',
+                'hash': 'ed1ad5185fbede55cd7fd506b3c6c699',
                 'size': 465600
             }, '/media/fat/linux/7za.gz')
 
