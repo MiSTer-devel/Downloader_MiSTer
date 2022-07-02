fee237885725f92039f36fab688ca26bf91f8248
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-07-02 16:08:43 +0200

    dont_download.zip to latest_release

--- .github/release.sh
+++ .github/release.sh
@@ -15,6 +15,8 @@ if [ ${CHANGES} -ge 1 ] ; then
   echo "There are changes to push."
   echo
   git push origin main
+  gh release create latest_release || true
+  gh release upload latest_release dont_download.zip --clobber
   echo
   echo "New dont_download.sh can be used."
   echo "::set-output name=NEW_RELEASE::yes"
