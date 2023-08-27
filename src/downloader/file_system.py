1f07262b589ec11736b70d82b31d29f3b4f13a05
José Manuel Barroso Galindo <theypsilon@gmail.com>
2023-08-27 19:44:44 +0200

    Reducing additional os.scandir call.

--- src/downloader/file_system.py
+++ src/downloader/file_system.py
@@ -203,8 +203,9 @@ class _FileSystem(FileSystem):
             if base_path is None:
                 self._existing_files.update(f.path for f in os.scandir(folder) if f.is_file())
             else:
-                self._existing_files.update(f.path.replace(base_path + '/', '') for f in os.scandir(os.path.join(base_path, folder)) if f.is_file())
-                self._existing_files.update(f.path for f in os.scandir(os.path.join(base_path, folder)) if f.is_file())
+                files = [f.path for f in os.scandir(os.path.join(base_path, folder)) if f.is_file()]
+                self._existing_files.update(f.path.replace(base_path + '/', '') for f in files)
+                self._existing_files.update(f.path for f in files)
 
     def read_file_contents(self, path):
         with open(self._path(path), 'r') as f:
