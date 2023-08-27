41eb521b4de117e5132afae2c908121f12774350
José Manuel Barroso Galindo <theypsilon@gmail.com>
2023-08-27 22:20:55 +0200

    debug script refactor.

--- src/debug.sh
+++ src/debug.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2022 José Manuel Barroso Galindo <theypsilon@gmail.com>
+# Copyright (c) 2021-2023 José Manuel Barroso Galindo <theypsilon@gmail.com>
 
 set -euo pipefail
 
@@ -9,43 +9,64 @@ if [ -f mister.pw ] ; then
     MISTER_PW=$(cat "mister.pw" | tr -d '[:space:]')
 fi
 
-TEMP_SCRIPT="$(mktemp)"
+send_build() {
+  local TEMP_SCRIPT="$(mktemp)"
+  DEBUG="${DEBUG:-true}" MISTER=true ./src/build.sh > "${TEMP_SCRIPT}"
+  chmod +x "${TEMP_SCRIPT}"
 
-if [[ "${1:-}" == 'store' ]] ; then
-  if [[ "${2:-}" == 'push' ]] ; then
-    zip -o downloader.json.zip downloader.json
-    sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no downloader.json.zip "root@${MISTER_IP}:/media/fat/Scripts/.config/downloader/downloader.json.zip"
-  elif [[ "${2:-}" == 'pull' ]] ; then
-    rm -f downloader.json.zip downloader.json
-    sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no "root@${MISTER_IP}:/media/fat/Scripts/.config/downloader/downloader.json.zip" downloader.json.zip
-    unzip -o downloader.json.zip
-  else
-    echo "Usage: $0 store [push|pull]"
-    exit 1
+  if [ -f dont_download.ini ] ; then
+    sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no dont_download.ini "root@${MISTER_IP}:/media/fat/downloader.ini"
   fi
-  exit 0
-elif [[ "${1:-}" == "touch" ]] ; then
-  if [[ "${2:-}" != "" ]] ; then
-    sshpass -p "${MISTER_PW}" ssh "root@${MISTER_IP}" "cd /media/fat; rm -f ${2}; touch ${2}"
-  else
-    echo "Usage: $0 touch <file>"
-    exit 1
-  fi
-  exit 0
-fi
+  sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no "${TEMP_SCRIPT}" "root@${MISTER_IP}:/media/fat/downloader.sh"
+  sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no downloader.sh "root@${MISTER_IP}:/media/fat/Scripts/downloader.sh"
+  rm "${TEMP_SCRIPT}"
+}
 
-DEBUG="${DEBUG:-true}" MISTER=true ./src/build.sh > "${TEMP_SCRIPT}"
-chmod +x "${TEMP_SCRIPT}"
+store_push() {
+  zip -o downloader.json.zip downloader.json
+  sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no downloader.json.zip "root@${MISTER_IP}:/media/fat/Scripts/.config/downloader/downloader.json.zip"
+}
 
-if [ -f dont_download.ini ] ; then
-  sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no dont_download.ini "root@${MISTER_IP}:/media/fat/downloader.ini"
-fi
-sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no "${TEMP_SCRIPT}" "root@${MISTER_IP}:/media/fat/downloader.sh"
-sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no downloader.sh "root@${MISTER_IP}:/media/fat/Scripts/downloader.sh"
-rm "${TEMP_SCRIPT}"
+store_pull() {
+  rm -f downloader.json.zip downloader.json
+  sshpass -p "${MISTER_PW}" scp -o StrictHostKeyChecking=no "root@${MISTER_IP}:/media/fat/Scripts/.config/downloader/downloader.json.zip" downloader.json.zip
+  unzip -o downloader.json.zip
+}
 
-if [[ "${1:-}" == 'run' ]] ; then
-  sshpass -p "${MISTER_PW}" ssh -o StrictHostKeyChecking=no "root@${MISTER_IP}" "/media/fat/downloader.sh ${2:-}"
-else
-  echo "OK"
-fi
+touch_file() {
+  sshpass -p "${MISTER_PW}" ssh "root@${MISTER_IP}" "cd /media/fat; rm -f ${1}; touch ${1}"
+}
+
+run_command() {
+  send_build
+  sshpass -p "${MISTER_PW}" ssh -o StrictHostKeyChecking=no "root@${MISTER_IP}" "/media/fat/downloader.sh ${1:-}"
+}
+
+run_launcher() {
+  send_build
+  sshpass -p "${MISTER_PW}" ssh -o StrictHostKeyChecking=no "root@${MISTER_IP}" "/media/fat/Scripts/downloader.sh ${1:-}"
+}
+
+case "${1:-}" in
+  'store')
+    case "${2:-}" in
+      'push') store_push ;;
+      'pull') store_pull ;;
+      *) echo "Usage: $0 store [push|pull]"; exit 1 ;;
+    esac
+    ;;
+  'touch')
+    if [[ -n "${2:-}" ]]; then
+      touch_file "$2"
+    else
+      echo "Usage: $0 touch <file>"
+      exit 1
+    fi
+    ;;
+  'run') run_command "${2:-}" ;;
+  'launcher') run_launcher "${2:-}" ;;
+  *)
+    send_build
+    echo "OK"
+    ;;
+esac
