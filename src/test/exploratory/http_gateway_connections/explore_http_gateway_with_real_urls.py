738295ea5b04b87f2e9103f74b8b30e2de6048fe
José manuel Barroso Galindo <theypsilon@gmail.com>
2025-01-21 03:22:43 +0100

    Added http redirect cache.

--- src/test/exploratory/http_gateway_connections/explore_http_gateway_with_real_urls.py
+++ src/test/exploratory/http_gateway_connections/explore_http_gateway_with_real_urls.py
@@ -18,10 +18,14 @@
 # https://github.com/MiSTer-devel/Downloader_MiSTer
 
 import os
+import signal
 import ssl
+import threading
 import time
+from concurrent.futures import ThreadPoolExecutor, Future
 from pathlib import Path
 from shutil import copyfileobj
+from typing import List
 
 from downloader.logger import PrintLogger
 from downloader.http_gateway import HttpGateway
@@ -40,35 +44,85 @@ urls = [
     'https://reddit.com/r/MiSTerFPGA/',
     'http://misterfpga.org/',
     'https://www.misterfpga.org/',
-    'https://archive.org/download/78_april-showers_al-jolson-silvers_gbia0006248b/_78_april-showers_al-jolson-silvers_gbia0006248b_02_2.3_CT_EQ.flac',
-    'https://archive.org/download/78_april-showers_al-jolson-silvers_gbia0006248b/_78_april-showers_al-jolson-silvers_gbia0006248b_02_2.3_CT_EQ.mp3',
-    'https://archive.org/download/78_april-showers_al-jolson-silvers_gbia0006248b/_78_april-showers_al-jolson-silvers_gbia0006248b_02_2.3_CT_EQ.png',
+    'https://archive.org/download/78_on-the-alamo_isham-jones-harry-radermans-orchestra_gbia0000908b/On%20The%20Alamo%20-%20Isham%20Jones%20-%20Harry%20Radermans%20Orchestra-restored_whisper_asr.json',
+    'https://archive.org/download/78_on-the-alamo_isham-jones-harry-radermans-orchestra_gbia0000908b/On%20The%20Alamo%20-%20Isham%20Jones%20-%20Harry%20Radermans%20Orchestra-restored.mp3',
+    'https://archive.org/download/78_on-the-alamo_isham-jones-harry-radermans-orchestra_gbia0000908b/On%20The%20Alamo%20-%20Isham%20Jones%20-%20Harry%20Radermans%20Orchestra-restored.png',
     'https://archive.org/download/publicmovies212/Bees_Buzz.webm',
     'https://archive.org/download/publicmovies212/Charlie_Chaplin_Caught_in_a_Caberet.mp4',
 ]
 
 
 def main() -> None:
-    logger = PrintLogger.make_configured({'verbose': True, 'start_time': time.time()})
-    dir_path = f'{os.path.dirname(os.path.realpath(__file__))}/delme'
-    os.makedirs(dir_path, exist_ok=True)
+    signals = [signal.SIGINT, signal.SIGTERM, signal.SIGHUP, signal.SIGQUIT]
+    signal_handlers = [(s, signal.getsignal(s)) for s in signals]
 
-    start = time.time()
-    with HttpGateway(ssl_ctx=ssl.create_default_context(), timeout=180, logger=logger) as gateway:
-        for input_url in urls * 20:
-            try:
-                with gateway.open(input_url) as (url, res):
-                    if res.status == 200:
-                        with open(f'{dir_path}/{Path(url).name[-30:]}', 'wb') as out_file:
-                            copyfileobj(res, out_file)
-            except Exception as e:
-                print(f'Unexpected exception during {input_url}!')
-                raise e
+    futures: List[Future[None]] = []
+    interrupted = False
+    cancelled = 0
+    def cleanup(interrupt: bool):
+        nonlocal interrupted, futures, cancelled
+        if interrupt: print('\n\n>>>>>>>>>>>>> INTERRUPTED!! CLEANING UP!\n\n')
 
-    end = time.time()
-    print()
-    print()
-    print(f'Time: {end - start}s')
+        for fut in futures:
+            if fut.done():
+                e = fut.exception()
+                if e is not None:
+                    print(e)
+            elif not fut.running():
+                if fut.cancel():
+                    cancelled += 1
+
+        futures = []
+        interrupted = True
+
+    def handle_interrupt(signum, frame, phandler):
+        cleanup(True)
+        if phandler: phandler(signum, frame)
+
+    for sig, cb in signal_handlers:
+        signal.signal(sig, lambda s, fr: handle_interrupt(s, fr, cb))
+
+    try:
+        logger = PrintLogger.make_configured({'verbose': True, 'start_time': time.time()})
+        dir_path = f'{os.path.dirname(os.path.realpath(__file__))}/delme'
+        os.makedirs(dir_path, exist_ok=True)
+
+        start = time.time()
+        with HttpGateway(ssl_ctx=ssl.create_default_context(), timeout=180, logger=logger) as gateway:
+            with ThreadPoolExecutor(max_workers=20) as thread_executor:
+                futures = [thread_executor.submit(fetch_url, gateway, input_url, dir_path) for input_url in urls * 20]
+
+                while len(futures) > 0 and not interrupted:
+                    next_futures = []
+                    for f in futures:
+                        if f.done():
+                            future_exception = f.exception()
+                            if future_exception is not None:
+                                print(future_exception)
+                        else:
+                            next_futures.append(f)
+
+                    futures = next_futures
+                    time.sleep(0.5)
+
+                cleanup(False)
+
+        end = time.time()
+        print()
+        print()
+        print(f'Time: {end - start}s')
+        print(f'Cancelled {cancelled} tasks.')
+
+    finally:
+        for sig, cb in signal_handlers:
+            signal.signal(sig, cb)
+
+
+def fetch_url(gateway: HttpGateway, input_url: str, dir_path: str):
+    with gateway.open(input_url) as (url, res):
+        if res.status == 200:
+            with open(f'{dir_path}/{threading.get_ident()}_{Path(url).name[-30:]}', 'wb') as out_file:
+                copyfileobj(res, out_file)
 
 
 if __name__ == '__main__':
