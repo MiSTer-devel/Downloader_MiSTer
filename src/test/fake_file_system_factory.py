d73cbd0f3c82a33b6ead99e6c4ba1bf753d6eded
José manuel Barroso Galindo <theypsilon@gmail.com>
2025-02-12 00:24:36 +0100

    Fixed bug.

--- src/test/fake_file_system_factory.py
+++ src/test/fake_file_system_factory.py
@@ -169,7 +169,7 @@ class FakeFileSystem(ProductionFileSystem):
     def read_file_contents(self, path):
         return self.state.files[self._path(path)]['content']
 
-    def precache_is_file_with_folders(self, _folders: list[str], _recheck: bool = False):
+    def precache_is_file_with_folders(self, _folders: list[str], recheck: bool = False):
         pass
 
     def write_file_contents(self, path, content):
