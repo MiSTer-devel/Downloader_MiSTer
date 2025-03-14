1e34c34068cfa5b5d8d6952fa0dc013b6c38ec83
José manuel Barroso Galindo <theypsilon@gmail.com>
2025-03-14 20:35:39 +0100

    Temp fix on Release process.

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2025 José Manuel Barroso Galindo <theypsilon@gmail.com>
+# Copyright (c) 2021-2022 José Manuel Barroso Galindo <theypsilon@gmail.com>
 
 set -euo pipefail
 
 DOWNLOADER_ZIP="downloader.zip"
 TEMP_ZIP2="${ZIP_FILE:-$(mktemp -u).zip}"
 BIN="/tmp/dont_download.zip"
+UUDECODE_CMD=$({ [[ "${MISTER:-false}" == "false" ]] && [[ "$(uname -s)" == "Darwin" ]] ; } && echo "uudecode -p" || echo "uudecode -o -")
 TEMPDIR="$(mktemp -d)"
 
 pin_metadata() {
@@ -17,12 +18,7 @@ cd src
 cp -r downloader "${TEMPDIR}/downloader"
 echo "default_commit = '$(git rev-parse --short HEAD)'" > "${TEMPDIR}/commit.py"
 cp __main__.py "${TEMPDIR}/__main__.py"
-if [[ "${SKIP_REMOVALS:-false}" != "true" ]] ; then
-  find "${TEMPDIR}" -type f -name '*.py' -exec perl -i -0pe 's/"""(.*?)"""/""/sg; s/^\s*#.*\n//mg; s/^\s*\n//mg' {} +
-fi
-#if which strip-hints > /dev/null ; then
-#  find "${TEMPDIR}" -type f -name '*.py' -exec strip-hints --inplace {} \; 2> /dev/null
-#fi
+find "${TEMPDIR}" -type f -name '*.py' -exec sed -i.bak -E '/^\s*#/d;/^$/d' {} +; find . -name '*.bak' -exec rm {} +
 find "${TEMPDIR}" -type f ! -name '*.py' -exec rm -f {} +
 find "${TEMPDIR}" -type f -iname "*.py" -print0 | while IFS= read -r -d '' file ; do pin_metadata "${file}" ; done
 pushd "${TEMPDIR}" >/dev/null 2>&1
@@ -38,12 +34,12 @@ cat <<-EOF
 #!/usr/bin/env bash
 set -euo pipefail
 export DOWNLOADER_LAUNCHER_PATH="\${DOWNLOADER_LAUNCHER_PATH:-\${0}}"
-tail -n +8 "\${0}" | xzcat -d -c > "${BIN}"
+${UUDECODE_CMD} "\${0}" | xzcat -d -c > "${BIN}"
 chmod a+x "${BIN}"
 "${BIN}" "\${1:-}"
 exit 0
 EOF
 
-xzcat -z < "${TEMP_ZIP2}"
+uuencode - < <(xzcat -z < "${TEMP_ZIP2}")
 
 rm -rf "${TEMPDIR}" >/dev/null 2>&1
