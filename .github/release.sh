3fff3bd8640ee453cd90a6f7ad7f544cd39d94c6
José manuel Barroso Galindo <theypsilon@gmail.com>
2025-03-15 03:32:33 +0100

    Properly fixing the release process.

--- .github/release.sh
+++ .github/release.sh
@@ -5,12 +5,12 @@ import subprocess
 import os
 import time
 
-subprocess.run(['git', 'add', 'dont_download.sh'], check=True)
+subprocess.run(['git', 'add', 'dont_download.sh', 'latest.id'], check=True)
 subprocess.run(['git', 'commit', '-m', 'BOT: New dont_download.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 subprocess.run(['git', 'fetch', 'origin', 'main'], check=True)
 
-diff_output = subprocess.run(['git', 'diff', 'main:dont_download.sh', 'origin/main:dont_download.sh'], capture_output=True, text=True, check=True).stdout
+diff_output = subprocess.run(['git', 'diff', 'main:latest.id', 'origin/main:latest.id'], capture_output=True, text=True, check=True).stdout
 lines = diff_output.split('\n')
 changes = [line for line in lines if line.startswith('+') or line.startswith('-')]
 changes = [line for line in changes if not line.startswith('+++') and not line.startswith('---') and 'export COMMIT' not in line]
@@ -26,8 +26,7 @@ if len(changes) >= 1:
     has_latest = subprocess.run(['gh', 'release', 'list'], capture_output=True, text=True)
     if "latest" not in has_latest.stdout:
         subprocess.run(['gh', 'release', 'create', 'latest'], stderr=subprocess.DEVNULL)
-
-    time.sleep(15)
+        time.sleep(15)
 
     result = subprocess.run(['md5sum', 'dont_download.zip'], capture_output=True, check=True, text=True)
     with open('dont_download.zip.md5', 'w') as f:
--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2022 José Manuel Barroso Galindo <theypsilon@gmail.com>
+# Copyright (c) 2021-2025 José Manuel Barroso Galindo <theypsilon@gmail.com>
 
 set -euo pipefail
 
 DOWNLOADER_ZIP="downloader.zip"
 TEMP_ZIP2="${ZIP_FILE:-$(mktemp -u).zip}"
 BIN="/tmp/dont_download.zip"
-UUDECODE_CMD=$({ [[ "${MISTER:-false}" == "false" ]] && [[ "$(uname -s)" == "Darwin" ]] ; } && echo "uudecode -p" || echo "uudecode -o -")
 TEMPDIR="$(mktemp -d)"
 
 pin_metadata() {
@@ -16,13 +15,23 @@ pin_metadata() {
 cd src
 
 cp -r downloader "${TEMPDIR}/downloader"
-echo "default_commit = '$(git rev-parse --short HEAD)'" > "${TEMPDIR}/commit.py"
+if [[ "${SKIP_COMMIT:-false}" != "true" ]] ; then
+  echo "default_commit = '$(git rev-parse --short HEAD)'" > "${TEMPDIR}/commit.py"
+  COMMIT_FILE="commit.py"
+else
+  COMMIT_FILE=""
+fi
 cp __main__.py "${TEMPDIR}/__main__.py"
-find "${TEMPDIR}" -type f -name '*.py' -exec sed -i.bak -E '/^\s*#/d;/^$/d' {} +; find . -name '*.bak' -exec rm {} +
+if [[ "${SKIP_REMOVALS:-false}" != "true" ]] ; then
+  find "${TEMPDIR}" -type f -name '*.py' -exec perl -i -0pe 's/"""(.*?)"""/""/sg; s/^\s*#.*\n//mg; s/^\s*\n//mg' {} +
+fi
+#if which strip-hints > /dev/null ; then
+#  find "${TEMPDIR}" -type f -name '*.py' -exec strip-hints --inplace {} \; 2> /dev/null
+#fi
 find "${TEMPDIR}" -type f ! -name '*.py' -exec rm -f {} +
-find "${TEMPDIR}" -type f -iname "*.py" -print0 | while IFS= read -r -d '' file ; do pin_metadata "${file}" ; done
+find "${TEMPDIR}" -type f -print0 | while IFS= read -r -d '' file ; do pin_metadata "${file}" ; done
 pushd "${TEMPDIR}" >/dev/null 2>&1
-zip -q -0 -D -X -A -r "${DOWNLOADER_ZIP}" __main__.py commit.py downloader -x "*/__pycache__/*"
+zip -q -0 -D -X -A -r "${DOWNLOADER_ZIP}" __main__.py "${COMMIT_FILE}" downloader -x "*/__pycache__/*"
 pin_metadata "${DOWNLOADER_ZIP}"
 echo '#!/usr/bin/env python3' | cat - "${DOWNLOADER_ZIP}" > "${TEMP_ZIP2}"
 pin_metadata "${TEMP_ZIP2}"
@@ -34,12 +43,12 @@ cat <<-EOF
 #!/usr/bin/env bash
 set -euo pipefail
 export DOWNLOADER_LAUNCHER_PATH="\${DOWNLOADER_LAUNCHER_PATH:-\${0}}"
-${UUDECODE_CMD} "\${0}" | xzcat -d -c > "${BIN}"
+tail -n +8 "\${0}" | xzcat -d -c > "${BIN}"
 chmod a+x "${BIN}"
 "${BIN}" "\${1:-}"
 exit 0
 EOF
 
-uuencode - < <(xzcat -z < "${TEMP_ZIP2}")
+xzcat -z < "${TEMP_ZIP2}"
 
 rm -rf "${TEMPDIR}" >/dev/null 2>&1
