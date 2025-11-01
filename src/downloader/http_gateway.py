1b5c021616863b11deaa6b6caaa9bdfce8afef41
José Manuel Barroso Galindo <theypsilon@gmail.com>
2025-11-02 00:29:01 +0100

    Fix import order by adding socket import in a lower part

--- src/downloader/http_gateway.py
+++ src/downloader/http_gateway.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021-2025 José Manuel Barroso Galindo <theypsilon@gmail.com>
-import socket
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -16,6 +16,7 @@ import socket
 # You can download the latest version of this tool from:
 # https://github.com/MiSTer-devel/Downloader_MiSTer
 
+import socket
 import ssl
 import sys
 import threading
